5f0576802462cf22af281726c2854f532d86cee2
Elias Lima <sci.eliaslima@gmail.com>
2024-07-11 06:04:39 -0300

    feat: create payment options component

--- src/components/PaymentOptions/index.tsx
+++ src/components/PaymentOptions/index.tsx
@@ -1,113 +1,189 @@
 import React, { useState } from 'react';
 import {
-  Container,
-  Title,
-  PaymentOptionsContainer,
-  OptionContainer,
-  SelectedOption,
-  OptionContent,
-  OptionLabel,
-  OptionTotal,
-  OptionInterest,
-  OptionCheckbox,
-  CheckboxIcon,
-  CashbackContainer,
-  CashbackIcon,
-  CashbackText,
-  CashbackValue,
-  SecurityText,
+    Container,
+    Title,
+    PaymentOptionsContainer,
+    OptionContainer,
+    OptionContent,
+    OptionTotal,
+    OptionCheckbox,
+    OptionTotalWrapper,
+    CheckboxIcon,
+    CashbackContainer,
+    FirstOptionContainer,
+    CashbackText,
+    OptionQuote,
+    OptionQuoteTotal,
+    PaymentOptionsTitle,
+    PaymentOptionsTitleWrapper,
+    ButtonWrapper,
 } from './styles';
+
 import { TouchableOpacity } from 'react-native';
+import { CashBackSticker } from '../CashBackSticker';
+import { DefaultButton } from '../DefaultButton';
+import { useNavigation } from '@react-navigation/native';
+import { propsNavigationStack } from '../../routes/Models';
+import { SecurityFooter } from '../SecurityFooter';
+
+export interface PixQuoteType {
+    id: number;
+    quote: number;
+    quote_value: string;
+    total: string;
+    cashback?: string;
+    cashbackValue?: string;
+    bestOption?: boolean;
+}
 
 const PaymentOptions = () => {
-  const [selectedOption, setSelectedOption] = useState<number | null>(null);
 
-  const paymentOptions = [
-    {
-      id: 1,
-      label: '1x R$ 30.500,00',
-      total: 'R$ 30.500,00',
-      cashback: 'Ganhe 3% de Cashback',
-      cashbackValue: 'R$ 300,00',
-    },
-    {
-      id: 2,
-      label: '2x R$ 15.300,00',
-      total: 'R$ 30.600,00',
-      interest: null,
-    },
-    {
-      id: 3,
-      label: '3x R$ 10.196,66',
-      total: 'R$ 30.620,00',
-      interest: null,
-    },
-    {
-      id: 4,
-      label: '4x R$ 7.725,00',
-      total: 'R$ 30.900,00',
-      interest: '-3% de juros: Melhor opção de parcelamento',
-    },
-    {
-      id: 5,
-      label: '5x R$ 6.300,00',
-      total: 'R$ 31.500,00',
-      interest: null,
-    },
-    {
-      id: 6,
-      label: '6x R$ 5.283,33',
-      total: 'R$ 31.699,98',
-      interest: null,
-    },
-    {
-      id: 7,
-      label: '7x R$ 4.542,85',
-      total: 'R$ 31.800,00',
-      interest: null,
-    },
-  ];
+    const [selectedOption, setSelectedOption] = useState<number | null>(1);
+    const navigation = useNavigation<propsNavigationStack>();
 
-  const handleOptionPress = (optionId: number) => {
-    setSelectedOption(optionId);
-  };
+    const uniquePix: PixQuoteType =
+    {
+        id: 1,
+        quote: 1,
+        quote_value: 'R$ 30.500,00',
+        total: 'R$ 30.500,00',
+        cashback: 'Ganhe 3% de Cashback',
+        cashbackValue: 'R$ 300,00',
+    };
 
-  return (
-    <Container>
-      <Title>João, como você quer pagar?</Title>
-      <PaymentOptionsContainer>
-        {paymentOptions.map((option) => (
-          <TouchableOpacity
-            key={option.id}
-            onPress={() => handleOptionPress(option.id)}
-          >
-            <OptionContainer selected={selectedOption === option.id}>
-              <OptionContent>
-                <OptionLabel>{option.label}</OptionLabel>
-                <OptionTotal>Total: {option.total}</OptionTotal>
-                {option.interest && (
-                  <OptionInterest>{option.interest}</OptionInterest>
-                )}
-                {option.cashback && (
-                  <CashbackContainer>
-                    {/* <CashbackIcon source={require('./cashback-icon.png')} /> */}
-                    <CashbackText>{option.cashback}</CashbackText>
-                    <CashbackValue>{option.cashbackValue}</CashbackValue>
-                  </CashbackContainer>
-                )}
-              </OptionContent>
-              <OptionCheckbox>
-                {/* {selectedOption === option.id && (
-                  <CheckboxIcon source={require('./checkbox-checked.png')} />
-                )} */}
-              </OptionCheckbox>
-            </OptionContainer>
-          </TouchableOpacity>
-        ))}
-      </PaymentOptionsContainer>
-      <SecurityText>Pagamento 100% seguro via: Woovi</SecurityText>
-    </Container>
-  );
+    const pixQuotes = [
+        {
+            id: 2,
+            quote: 2,
+            quote_value: 'R$ 15.300,00',
+            total: 'R$ 30.600,00',
+        },
+        {
+            id: 3,
+            quote: 3,
+            quote_value: 'R$ 10.196,66',
+            total: 'R$ 30.620,00',
+        },
+        {
+            id: 4,
+            quote: 4,
+            quote_value: 'R$ 7.725,00',
+            total: 'R$ 31.500,00',
+            bestOption: true
+        },
+        {
+            id: 5,
+            quote: 5,
+            quote_value: 'R$ 6.300,00',
+            total: 'R$ 30.900,00',
+        },
+        {
+            id: 6,
+            quote: 6,
+            quote_value: 'R$ 5.283,33',
+            total: 'R$ 31.699,98',
+        },
+        {
+            id: 7,
+            quote: 7,
+            quote_value: 'R$ 4.542,85',
+            total: 'R$ 31.800,00',
+        },
+    ] as PixQuoteType[];
+
+    const handleShowPaymentDetails = () => {
+        if (selectedOption === 1) {
+            navigation.navigate('DetailMethodScreen', uniquePix);
+        } else {
+            const quote = pixQuotes.find(item => item.id === selectedOption) as PixQuoteType;
+            navigation.navigate('DetailMethodScreen', quote);
+        }
+    }
+
+    const handleOptionPress = (optionId: number) => {
+        setSelectedOption(optionId);
+    };
+
+    return (
+        <Container>
+            <Title>Elias, como você quer pagar?</Title>
+            <PaymentOptionsContainer>
+                <PaymentOptionsTitleWrapper>
+                    <PaymentOptionsTitle>
+                        Pix
+                    </PaymentOptionsTitle>
+                </PaymentOptionsTitleWrapper>
+                <TouchableOpacity
+                    key={uniquePix.id}
+                    onPress={() => handleOptionPress(uniquePix.id)}>
+                    <FirstOptionContainer
+                        selected={selectedOption === uniquePix.id}>
+                        <OptionContent>
+                            <OptionTotalWrapper>
+                                <OptionCheckbox>
+                                    <OptionQuote>{uniquePix.quote}x</OptionQuote>
+                                    <OptionQuoteTotal>{uniquePix.total}</OptionQuoteTotal>
+                                </OptionCheckbox>
+
+                                {selectedOption === uniquePix.id ? 
+                                    <CheckboxIcon source={require('./../../../assets/checkbox-checked.png')} /> : <CheckboxIcon source={require('./../../../assets/ellipse-raw.png')} />
+                                }
+                            </OptionTotalWrapper>
+                            {uniquePix.cashback && (
+                                <CashbackContainer>
+                                    <CashbackText>{uniquePix.cashback}</CashbackText>
+                                    <CashBackSticker cashBackValue={uniquePix.cashbackValue} />
+                                </CashbackContainer>
+                            )}
+
+                        </OptionContent>
+                    </FirstOptionContainer>
+                </TouchableOpacity>
+            </PaymentOptionsContainer>
+            <PaymentOptionsContainer>
+                <PaymentOptionsTitleWrapper>
+                    <PaymentOptionsTitle>
+                        Pix Parcelado
+                    </PaymentOptionsTitle>
+                </PaymentOptionsTitleWrapper>
+                {pixQuotes.map((option) => (
+                    <TouchableOpacity
+                        key={option.id}
+                        onPress={() => handleOptionPress(option.id)}>
+                        <OptionContainer
+                            selected={selectedOption === option.id}
+                            lastOption={option.id === pixQuotes.length + 1}
+                            firstOption={option.id === 2}>
+                            <OptionContent>
+                                <OptionTotalWrapper>
+                                    <OptionCheckbox>
+                                        <OptionQuote>{option.quote}x</OptionQuote>
+                                        <OptionQuoteTotal>{option.quote_value}</OptionQuoteTotal>
+                                    </OptionCheckbox>
+                        
+                                    {selectedOption === option.id ? 
+                                    <CheckboxIcon source={require('./../../../assets/checkbox-checked.png')} /> : <CheckboxIcon source={require('./../../../assets/ellipse-raw.png')} />
+                                }
+                                </OptionTotalWrapper>
+                                {option.total && (
+                                    <OptionTotal>Total: {option.total}</OptionTotal>
+                                )}
+                                {option.bestOption && (
+                                    <CashBackSticker bestOption={option.bestOption} />
+                                )}
+                            </OptionContent>
+
+                        </OptionContainer>
+                    </TouchableOpacity>
+                ))}
+            </PaymentOptionsContainer>
+            {selectedOption &&
+                <ButtonWrapper>
+                    <DefaultButton title="Continuar" onPress={handleShowPaymentDetails} />
+                </ButtonWrapper>}
+            <SecurityFooter />
+        </Container>
+    );
 };
 
 export default PaymentOptions;
--- src/components/PaymentOptions/styles.ts
+++ src/components/PaymentOptions/styles.ts
@@ -1,8 +1,15 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface OptionContainerProps {
+  selected: boolean;
+  lastOption?: boolean;
+  firstOption?: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
-  padding: 20px;
+  padding: 16px;
 `;
 
 export const Title = styled.Text`
@@ -14,51 +21,86 @@ export const Title = styled.Text`
 `;
 
 export const PaymentOptionsContainer = styled.View`
-  margin-bottom: 20px;
+  width: 100%;
 `;
 
-interface OptionContainerProps {
-  selected: boolean;
-}
+export const FirstOptionContainer = styled.View<OptionContainerProps>`
+   flex-direction: row;
+    align-items: center;
+    padding: 15px;
+    background-color: ${(props) => (props.selected ? 'rgba(3, 214, 157, .05)' : '#fff')};
+    border: 1px #E5E5E5;
+    border-radius: 10px;
+    margin-bottom: 20px;
+    ${(props) => props.selected && css`
+    border: 2px #03D69D;
+  `};
+`
 
 export const OptionContainer = styled.View<OptionContainerProps>`
   flex-direction: row;
   align-items: center;
   padding: 15px;
-  background-color: ${(props) => (props.selected ? '#f0f0f0' : '#fff')};
-  border-radius: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #E5E5E5;
-`;
+  background-color: ${(props) => (props.selected ? 'rgba(3, 214, 157, .05)' : '#fff')};
+  border: 1px #E5E5E5;
+  
+  ${(props) => props.selected && css`
+    border: 2px #03D69D;
+  `};
+  
+  ${(props) => props.selected && props.lastOption && css`
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        border: 2px #03D69D;
+        border-bottom-color: #03D69D;
+    `};
 
-export const SelectedOption = styled(OptionContainer)`
-  background-color: #f0f0f0;
+    ${(props) => props.lastOption && css`
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+    `};
+
+    ${(props) => props.firstOption && css`
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+    `};
+
 `;
 
 export const OptionContent = styled.View`
   flex: 1;
+  gap: 1px;
 `;
 
-export const OptionLabel = styled.Text`
-  font-size: 16px;
-  font-weight: bold;
-  margin-bottom: 5px;
+export const OptionTotalWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  justify-content: space-between;
+`;
+
+export const OptionQuote = styled.Text`
+  font-size: ${RFValue(16)}px;
+  font-family: 'Nunito_800ExtraBold';
+  color: #4D4D4D;
+`;
+
+export const OptionQuoteTotal = styled.Text`
+  font-size: ${RFValue(16)}px;
+  font-family: 'Nunito_600SemiBold';
+  color: #4D4D4D;
 `;
 
 export const OptionTotal = styled.Text`
-  font-size: 14px;
+  font-size: ${RFValue(12)}px;
   margin-bottom: 5px;
-`;
-
-export const OptionInterest = styled.Text`
-  font-size: 12px;
-  color: #007bff; /* Change to your desired color */
+  font-family: 'Nunito_600SemiBold';
+  color: #AFAFAF;
 `;
 
 export const OptionCheckbox = styled.View`
-  width: 30px;
-  height: 30px;
-  justify-content: center;
+  gap: 4px;
+  flex-direction: row;
   align-items: center;
 `;
 
@@ -68,30 +110,37 @@ export const CheckboxIcon = styled.Image`
 `;
 
 export const CashbackContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  margin-top: 5px;
+  flex-direction: column;
+  gap: 6px;
 `;
 
-export const CashbackIcon = styled.Image`
-  width: 16px;
-  height: 16px;
-  margin-right: 5px;
-`;
 
 export const CashbackText = styled.Text`
   font-size: 12px;
-  color: #007bff; /* Change to your desired color */
+  color: #03D69D;
 `;
 
-export const CashbackValue = styled.Text`
-  font-size: 12px;
-  font-weight: bold;
-  color: #007bff; /* Change to your desired color */
+export const PaymentOptionsTitleWrapper = styled.View`
+  border-radius: 100px;
+  justify-content: center;
+  align-items: center;
+  position: 'absolute';
+  top: 9px;
+  left: 9px;
+  z-index: 1;
+  max-width: 150px;
+  background-color: #E5E5E5;
 `;
 
-export const SecurityText = styled.Text`
-  font-size: 12px;
-  text-align: center;
-  color: #777; /* Change to your desired color */
+export const PaymentOptionsTitle = styled.Text`
+  font-family: 'Nunito_800ExtraBold';
+  font-size: 18px;
+  color: #4D4D4D;
+`;
+
+
+export const ButtonWrapper = styled.View`
+  margin-top: 32px;
+  align-items: center;
+  width: 100%;
 `;
