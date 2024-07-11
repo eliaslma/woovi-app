9e6f2895687bce7d5f1d8acf4849a575e7782be7
Elias Lima <sci.eliaslima@gmail.com>
2024-07-11 06:05:16 -0300

    feat: create payment method screen

--- src/screens/PaymentMethodScreen/styles.ts
+++ src/screens/PaymentMethodScreen/styles.ts
@@ -4,3 +4,15 @@ export const Container = styled.View`
     flex: 1;
     background-color: #FFFFFF;
 `;
+
+export const LogoWrapper = styled.View`
+    width: 100%;
+    align-items: center;
+    margin-top: 8px;
+`;
+
+export const Logo = styled.Image`
+  width: 123px;
+  height: 36px;
+  text-align: center;
+`;
