794a216d559294b0b263b39ff02d481f44c41d8e
Elias Lima <sci.eliaslima@gmail.com>
2024-07-11 06:01:36 -0300

    feat: create security footer

---
+++ src/components/SecurityFooter/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    width: 100%;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  gap: 6px;
+  margin-top: 40px;
+`;
+
+export const SecurityIcon = styled.Image``;
+
+export const SecurityText = styled.Text`
+  font-size: 14px;
+  text-align: center;
+  color: #B2B2B2;
+  font-family: 'Nunito_600SemiBold';
+`;
