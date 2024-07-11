07e404f889051b23b178f749a222708599857178
Elias Lima <sci.eliaslima@gmail.com>
2024-07-11 06:00:29 -0300

    feat: create line divider

---
+++ src/components/LineDivider/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {
+    Line
+} from './styles';
+
+export function LineDivider(){
+    return(
+        <Line/>
+    );
+}
---
+++ src/components/LineDivider/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const Line = styled.View`
+    height: 1px;
+    margin: 19px;
+    background-color: #E5E5E5;
+`;
