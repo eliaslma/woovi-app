90f69ade847f7a5f45fecd62122e94721433a90a
Elias Lima <sci.eliaslima@gmail.com>
2024-07-11 06:05:56 -0300

    feat: create pay screen

--- src/screens/PayScreen/styles.ts
+++ src/screens/PayScreen/styles.ts
@@ -2,4 +2,32 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
+    background-color: #FFFFFF;
+`;
+
+export const LogoWrapper = styled.View`
+    width: 100%;
+    align-items: center;
+    margin-top: 8px;
+    margin-bottom: 40px;
+`;
+
+export const Logo = styled.Image`
+  width: 123px;
+  height: 36px;
+  text-align: center;
+`;
+
+export const FormWrapper = styled.View`
+    padding: 19px;
+    gap: 20px;
+`;
+
+export const CardSecurityWrapper = styled.View`
+    flex-direction: row;
+    gap: 22px;
+`;
+
+export const SelectQuote = styled.Text`
+text-align: center;
 `;
